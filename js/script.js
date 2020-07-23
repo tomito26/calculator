d0366142a3e6976e68ea36c1d9ecaa1dbdeda551
tomito26 <tommybwah@gmail.com>
2020-07-23 23:52:57 +0300

    modified the input field and used bootstrap form input and also modified the js by using jQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-
-function addition(){
-    let num1 = parseInt(document.getElementById('num1').value)
-    let num2 = parseInt(document.getElementById('num2').value)
-     alert(num1 + num2)
-}
+// Business(or backend) logi:
+let add = function(number1,number2){
+    return number1 + number2;
+};
+let subtract = function(number1,number2){
+    return number1 - number2;
+};
+let multiply = function(number1,number2){
+    return number1 * number2;
+};
+let divide= function(number1,number2){
+    return number1 / number2;
+};
+// Everything below this line is the user interface(or front-end) logic:
+$ (document).ready(function(){
+    $('form#add').submit(function(event){
+        event.preventDefault();
+        let number1 = parseInt($('#add1').val());
+        let number2 = parseInt($('#add2').val());
+        let result =add(number1,number2)
+        $('#output').text(result);
+    });
+});
